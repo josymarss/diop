c6e9e2334f815f6f5761eb5d01bf599d7298eca1
josymarss <sylvajosymar@gmail.com>
2024-10-01 02:36:31 +0100

    Initial Commit

---
+++ diop/diopApp.swift
@@ -0,0 +1,17 @@
+//
+//  diopApp.swift
+//  diop
+//
+//  Created by Josymarss on 01/10/24.
+//
+
+import SwiftUI
+
+@main
+struct diopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
